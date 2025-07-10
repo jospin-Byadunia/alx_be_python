91ab82f2ef272eded772a889a087437f73ac4096
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-10 10:04:09 +0200

    corrected input validation error

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
-currentAge = int(input("“How old are you?"))
+currentAge = input("“How old are you?")
 
-age = currentAge+25
+age = int(currentAge)+25
 
 print(f"In 2050, you will be {age} years old")
