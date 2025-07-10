7978348d1c26733c3eb977f848ddf32cc5c71e37
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-10 09:40:04 +0200

    completed task one by doing all the operations needed

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 =5
+number2 = 10
+
+print(f"Addition of {number1} and {number2} is ",number1 + number2)
+print(f"Subtraction of {number2} and {number1} is ", number2 - number1)
+print(f"Multiplication of {number1} and {number2} is ", number1 * number2)
