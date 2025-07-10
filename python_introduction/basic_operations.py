9a44577ce6af24e406b7dc2a662e16c44eb44137
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-10 09:45:12 +0200

    corrected numbering error

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,5 @@
-number1 =5
-number2 = 10
+number1 = 10
+number2 = 5
 
 print(f"Addition of {number1} and {number2} is ",number1 + number2)
 print(f"Subtraction of {number2} and {number1} is ", number2 - number1)
