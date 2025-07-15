be4ffe1fe63de41ffdcd1ba0d87cf38cfe7c3114
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-15 21:39:01 +0200

    corrected the pattern challenge

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,8 @@
 pattern_number = int(input("Enter the size of the pattern:"))
+num = 0
 
-for i in range(1, pattern_number + 1):
+while num < pattern_number:
+    num += 1
     for j in range(1, pattern_number + 1):
         print("*", end="")
     print()  # Move to the next line after each row
