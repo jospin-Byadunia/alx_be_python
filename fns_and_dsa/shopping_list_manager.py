e4f1e56dd066a79502772a61fbf55f919602d4ec
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-25 19:23:19 +0200

    corrected input choice to be number

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == '2':
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             shopping_list.remove(item)
             # Prompt for and remove an item
             pass
