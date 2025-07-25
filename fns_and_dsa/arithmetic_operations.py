fd1b402d3c4959394cf5b91616cd31c758d9a5f0
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-25 19:06:25 +0200

    corrected divide operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             return num1 * num2
         case 'divide':
-            if num2 == 0:
-                return "cannot devide a number by zero"
-            else:
+            if num2 > 0:
                 return num1 / num2
+            elif num2 == 0:
+                return "cannot devide a number by zero"
