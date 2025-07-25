684679b734485cf876110e19c3c7899275b4b1eb
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-25 19:00:42 +0200

    added arithmetic operation file and the main to call

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1,num2,operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return abs(num1 - num2)
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return "cannot devide a number by zero"
+            else:
+                return num1 / num2
