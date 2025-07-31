55099e2df822e9751ef68972e2e32046764f050e
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-31 22:53:39 +0200

    changed function names

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,11 +5,11 @@ class SimpleCalculatorTest(unittest.TestCase):
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(3,4),7)
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10,8),2)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2,6),12)
     def test_division(self):
         self.assertEqual(self.calc.divide(10,2), 5)
