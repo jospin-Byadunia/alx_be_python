862a3897d4063fff36e9f8e2103398b2f46eed7d
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-31 22:14:27 +0200

    added 0 default balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,7 +2,7 @@ class BankAccount:
     def __init__(self,account_balance):
         self.account_balance = account_balance
     
-    def deposit(self, amount):
+    def deposit(self, amount=0):
         self.account_balance += amount
         return self.account_balance
 
