9ac3f3be295ced677ff523008744c9ecc15ed70c
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-07-31 22:51:51 +0200

    created a simple testor for a calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,17 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class SimpleCalculatorTest(unittest.TestCase):
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+    def test_add(self):
+        self.assertEqual(self.calc.add(3,4),7)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10,8),2)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2,6),12)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10,2), 5)
+    def test_divide_by_zero(self):
+        self.assertEqual(self.calc.divide(12,0), None)
