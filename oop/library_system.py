f1a0417e620b60afeaaca768a50232143b6005d7
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-08-05 21:10:16 +0200

    library management system added

---
+++ oop/library_system.py
@@ -0,0 +1,34 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def get_details(self):
+        return f"Book: '{self.title}' by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+    def get_details(self):
+        return f"EBook: '{self.title}' by {self.author}, File Size: {self.file_size}MB"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+    def get_details(self):
+        return f"PrintBook: '{self.title}' by {self.author}, Pages: {self.page_count}"
+
+class Library():
+    def __init__(self):
+        self._books = []
+    
+    def add_book(self, book):
+        self._books.append(book)
+
+    def list_books(self):
+        if not self._books:
+            print("The library is empty.")
+        else:
+            for book in self._books:
+                print(book.get_details())
